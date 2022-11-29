baf139f47304854da164437be4f87adbc162d2c9
CeciliaBdev <cecilia.bernard@live.fr>
2022-11-29 21:02:49 +0100

    update package

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// import Modal from './lib/components/Modal'
-// import modalcss from './lib/components/modal.css'
-// export { Modal, modalcss }
+import Modal from './lib/components/Modal'
+import modalcss from './lib/components/modal.css'
+export { Modal, modalcss }
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './exemple.js'
-import { BrowserRouter } from 'react-router-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './exemple.js'
+// import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// )
